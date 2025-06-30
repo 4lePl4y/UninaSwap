d0c394d48f523e89142d7392c2f6a9df90ac78bd
4lePl4y <aletheplayer2@gmail.com>
2025-06-30 15:28:41 +0200

    Rimosso la variabile controller duplicata

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -18,7 +18,6 @@ import java.awt.event.MouseEvent;
 import controller.Controller;
 
 public class Login extends JFrame {
-	public Controller controller;
 	private Controller controller;
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
