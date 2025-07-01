9bea01d3190b6dd3ade98b6ca4c8709acbba4daf
Marco33-dev <97241498+Marco33-dev@users.noreply.github.com>
2025-07-01 20:47:18 +0200

    Update Controller.java

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,5 @@
 package controller;
-
+//
 import java.awt.EventQueue;
 
 import gui.*;
@@ -26,9 +26,15 @@ public class Controller {
 	}
 	
 	//METODI
-	public void onRegistratiClicked(){
+	public void onRegisterClicked(){
 		signUpFrame.setVisible(true);
 		loginFrame.setVisible(false);
+	}
+
+	public void onLoginClicked() {
+		signUpFrame.setVisible(false);
+		loginFrame.setVisible(true);
+		
 	}
 	
 }
