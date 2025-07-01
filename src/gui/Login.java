3ba27c55f7edfaa8ee50459b1540b1c8ae9b9980
4lePl4y <aletheplayer2@gmail.com>
2025-07-01 10:47:12 +0200

    Updated Login frame

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -1,7 +1,7 @@
 package gui;
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -9,18 +9,33 @@ import java.awt.Window.Type;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.Color;
 import javax.swing.JToggleButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Cursor;
+import java.awt.Dimension;
+
 import controller.Controller;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
+import javax.swing.BoxLayout;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.DropMode;
+import java.awt.Insets;
+import javax.swing.SwingConstants;
+import javax.swing.JSlider;
 
 public class Login extends JFrame {
-	private Controller controller;
+	public Controller controller;
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField userTxtField;
@@ -30,7 +45,8 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login(Controller controller) {
-		this.controller=controller; 
+		this.controller = controller; 
+		
 		setTitle("Unina Swap");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1080, 650);
@@ -38,53 +54,114 @@ public class Login extends JFrame {
 		contentPane.setForeground(new Color(0, 0, 0));
 		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new GridBagLayout());
 		
-		userTxtField = new JTextField();
-		userTxtField.setBounds(470, 132, 157, 33);
-		contentPane.add(userTxtField);
-		userTxtField.setColumns(10);
+		JLabel lblNewLabel = new JLabel("Login");
+		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
 		
-		JLabel userLabel = new JLabel("Username");
-		userLabel.setLabelFor(userTxtField);
-		userLabel.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		userLabel.setBounds(375, 137, 88, 20);
-		contentPane.add(userLabel);
 		
-		JLabel pswLabel = new JLabel("Password");
-		pswLabel.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		pswLabel.setBounds(385, 278, 78, 20);
-		contentPane.add(pswLabel);
 		
-		pswTxtField = new JPasswordField();
-		pswLabel.setLabelFor(pswTxtField);
-		pswTxtField.setBounds(470, 273, 157, 33);
-		contentPane.add(pswTxtField);
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		contentPane.add(panel, gbc);
+		panel.setPreferredSize(new Dimension(500, 350));
 		
-		JButton lgnButton = new JButton("LOGIN");
-		lgnButton.setBackground(new Color(255, 255, 255));
-		lgnButton.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		lgnButton.setBounds(504, 347, 104, 33);
-		contentPane.add(lgnButton);
+				
+				JLabel userLabel = new JLabel("Username");
+				userLabel.setBounds(167, 70, 62, 20);
+				userLabel.setLabelFor(userTxtField);
+				userLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+				panel.add(userLabel);
+				
+				JLabel pswLabel = new JLabel("Password");
+				pswLabel.setBounds(167, 140, 62, 20);
+				pswLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+				panel.add(pswLabel);
+				
+				pswTxtField = new JPasswordField();
+				pswTxtField.setBounds(167, 160, 160, 30);
+				pswLabel.setLabelFor(pswTxtField);
+				panel.add(pswTxtField);
+				
+				JButton lgnButton = new JButton("Login");
+				lgnButton.setBounds(167, 218, 160, 28);
+				lgnButton.setBackground(SystemColor.textHighlight);
+				lgnButton.setForeground(new Color(255, 255, 255));
+				lgnButton.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+				lgnButton.setBorderPainted(false);
+				lgnButton.setFocusPainted(false);
+				
+//		lgnButton.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//			}
+//		});
+				
+				lgnButton.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseEntered(MouseEvent e) {
+						lgnButton.setBackground(new Color(0, 102, 204));
+						lgnButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+					}
+				});
+				
+				lgnButton.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseExited(MouseEvent e) {
+						lgnButton.setBackground(SystemColor.textHighlight);
+						lgnButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+					}
+				});
+				
+				panel.add(lgnButton);
+				
+				JLabel SignUpLabel = new JLabel("Registrati");
+				SignUpLabel.setForeground(Color.DARK_GRAY);
+				SignUpLabel.setBounds(272, 299, 78, 17);
+				SignUpLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+				SignUpLabel.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						onRegistratiClicked();
+					}
+					@Override
+					public void mouseEntered(MouseEvent e) {
+						SignUpLabel.setForeground(Color.BLUE);
+						SignUpLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+					}
+					@Override
+					public void mouseExited(MouseEvent e) {
+						SignUpLabel.setForeground(Color.DARK_GRAY);
+				SignUpLabel.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+					}
+				});
+				
+				userTxtField = new JTextField();
+				userTxtField.setBounds(167, 90, 160, 30);
+				userTxtField.setColumns(10);
+				panel.add(userTxtField);
+				panel.add(SignUpLabel);
+				
+				JLabel lblNewLabel_1 = new JLabel("Non hai un account?");
+				lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+				lblNewLabel_1.setForeground(Color.DARK_GRAY);
+				lblNewLabel_1.setBounds(139, 301, 130, 13);
+				panel.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel = new JLabel("Non hai un account?");
-		lblNewLabel.setBounds(438, 429, 116, 16);
-		contentPane.add(lblNewLabel);
-		
-		JButton btnNewButton = new JButton("Registrati qui");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				onRegistratiClicked();
-			}
-		});
-		btnNewButton.setBounds(565, 424, 144, 26);
-		contentPane.add(btnNewButton);
+	
 		}
 	
 	//METODI
 	public void onRegistratiClicked(){
-		controller.onRegistratiClicked(); 
+		controller.onRegistratiClicked();
 	}
 }
