efc13d47fa2d20068c1c81b7126b9798bdc746f2
Marco33-dev <marcofesta105@gmail.com>
2025-08-03 12:27:25 +0200

    Update Studente.java

--- src/entities/studente/Studente.java
+++ src/entities/studente/Studente.java
@@ -1,5 +1,27 @@
 package entities.studente;
 
-public class Studente {
+import java.util.ArrayList;
+import entities.annuncio.Annuncio;
+import entities.offerta.Offerta;
+import entities.oggetto.Oggetto;
 
+public class Studente {
+	private String nome;
+	private String cognome;
+	private String eMail;
+	private String username;
+	private ArrayList<Oggetto> oggetti;
+	private ArrayList<Annuncio> annunci;
+	private ArrayList<Offerta> offerte;
+	
+	
+	public Studente(String nome, String cognome, String eMail, String username) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.eMail = eMail;
+		this.username = username;
+		this.oggetti = new ArrayList<Oggetto>();
+		this.annunci = new ArrayList<Annuncio>();
+		this.offerte = new ArrayList<Offerta>();
+	}
 }
