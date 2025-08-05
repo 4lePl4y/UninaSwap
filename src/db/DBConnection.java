b68bac96033cbb182bcd0749ea2b7d75c32f9667
Marco33-dev <marcofesta105@gmail.com>
2025-08-05 16:56:11 +0200

    Update DBConnection.java

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -10,26 +10,27 @@ public class DBConnection {
 	
 	private DBConnection() {};
 	
-	 public static DBConnection getDBConnection() {  
-	        if (dbcon == null) {
-	            dbcon = new DBConnection();
-	        }
+	public static DBConnection getDBConnection() {  
+		if (dbcon == null) {
+			dbcon = new DBConnection();
+		}
 	        
-	        return dbcon;
-	    }
+		return dbcon;
+	}
 	 
 	 public Connection getConnection() throws SQLException {
-		 if(conn == null || conn.isClosed()) {
-			 conn = DriverManager.getConnection(
-			  "jdbc:postgresql://aws-0-eu-central-2.pooler.supabase.com:5432/postgres?user=postgres.ukdoakobkjfbrymxtulr&password=UninaSwap"
-			 );
-
-	
+		 try {
+			 if(conn == null || conn.isClosed()) 
+				 conn = DriverManager.getConnection("jdbc:postgresql://aws-0-eu-central-2.pooler.supabase.com:5432/postgres", "postgres.ukdoakobkjfbrymxtulr", "UninaSwap");
+			 
+		 }
+		 
+		 catch (SQLException  e) {
+			 System.out.println("Errore di connessione al database: " + e.getMessage());
+			 throw e; 
 		 }
 		 
 		 return conn;
 	 }
-
-
-
+	 
 }
