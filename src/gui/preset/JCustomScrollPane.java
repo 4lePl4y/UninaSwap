a898107a3500fb79a7fd398256b815dc0251a76c
4lePl4y <aletheplayer2@gmail.com>
2025-09-20 11:12:06 +0200

    Modificato customScrollPane
    ora customScrollPane ha uno sfondo personalizzato ossia lo scrollPaneBackground.png, è comune a tutti gli scroll pane in quanto attributo statico

--- src/gui/preset/JCustomScrollPane.java
+++ src/gui/preset/JCustomScrollPane.java
@@ -2,9 +2,13 @@ package gui.preset;
 
 import gui.preset.presetJPanel.JCardsPane;
 
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
 import javax.swing.JScrollPane;
 
 import controller.Controller;
@@ -13,6 +17,7 @@ public class JCustomScrollPane<T> extends JScrollPane {
 	private static final long serialVersionUID = 1L;
 	private JCardsPane<T> cardsPane;
 	private Controller controller;
+	private static final Image BACKGROUND_IMAGE =new ImageIcon("src/img/scrollPaneBackground.png").getImage();
 	
 	public JCustomScrollPane(ArrayList<T> contents, Controller controller) {
 		this.controller = controller;
@@ -30,6 +35,16 @@ public class JCustomScrollPane<T> extends JScrollPane {
 				cardsPane.revalidate();
 				}
 		});
+		
+        setOpaque(false);
+        getViewport().setOpaque(false);
+		
+	}
+	
+	//metodo per disegnare lo sfondo personalizzato sullo scroll pane
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(BACKGROUND_IMAGE, 0, 0, getWidth(), getHeight(), this);
 	}
 	
 }
