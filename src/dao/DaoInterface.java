c73bc3975a4ff60f19ce2e54a2bd7ce1609612aa
Marco33-dev <marcofesta105@gmail.com>
2025-09-20 15:32:48 +0200

    Update DaoInterface.java

--- src/dao/DaoInterface.java
+++ src/dao/DaoInterface.java
@@ -1,5 +1,7 @@
 package dao;
 
+import exception.CustomSQLException;
+
 public interface DaoInterface<T> {
 	//ATTRIBUTI 
 	
@@ -8,7 +10,7 @@ public interface DaoInterface<T> {
 		throw new UnsupportedOperationException("Method not implemented");
 	}
 	
-	public void create(T object);
+	public void create(T object) throws CustomSQLException;
 	public void update(T object);
 	default public void delete(String id) {
 		throw new UnsupportedOperationException("Method not implemented");
