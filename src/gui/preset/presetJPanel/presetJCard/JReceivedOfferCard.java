1dd00177a58ab8481c830e37adcba9c402e1a6f8
Marco33-dev <marcofesta105@gmail.com>
2025-09-25 20:01:47 +0200

    Messi bottoni accetta/rifiuta

--- src/gui/preset/presetJPanel/presetJCard/JReceivedOfferCard.java
+++ src/gui/preset/presetJPanel/presetJCard/JReceivedOfferCard.java
@@ -2,18 +2,19 @@ package gui.preset.presetJPanel.presetJCard;
 
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 import controller.Controller;
 import entities.enumerazioni.Stato;
 import entities.offerta.Offerta;
-import gui.preset.presetJButton.JButtonWithBorder;
+import gui.preset.presetJButton.JAcceptButton;
+import gui.preset.presetJButton.JDeclineButton;
 
 public class JReceivedOfferCard extends JAbstractOfferCard {
     private static final long serialVersionUID = 1L;
@@ -26,11 +27,8 @@ public class JReceivedOfferCard extends JAbstractOfferCard {
             JPanel footer = new JPanel(new FlowLayout(FlowLayout.RIGHT, 10, 0));
             footer.setOpaque(false);
 
-            JButton acceptButton = new JButtonWithBorder("Accetta");
-            JButton declineButton = new JButtonWithBorder("Rifiuta");
-
-            acceptButton.setBackground(new Color(0, 150, 0));   // TODO: bottone accetta verde
-            declineButton.setBackground(new Color(200, 0, 0)); // TODO: bottone rifiuta rosso
+            JButton acceptButton = new JAcceptButton();
+            JButton declineButton = new JDeclineButton();
 
             acceptButton.addMouseListener(new MouseAdapter() {
                 @Override
@@ -48,7 +46,7 @@ public class JReceivedOfferCard extends JAbstractOfferCard {
 
             footer.add(acceptButton);
             footer.add(declineButton);
-
+            footer.setBorder(BorderFactory.createEmptyBorder(15, 0, 0, 0)); 
             add(footer, BorderLayout.SOUTH);
         }
     }
